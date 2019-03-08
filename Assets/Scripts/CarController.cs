06c176bf1d099d9a59cc2661757ef7cc9653754b
Rajat Gupta <imrajatgupta@gmail.com>
2019-03-07 22:09:49 -0500

    Track loops, car moves

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float carSpeed;
+    Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // current position gets copied to hte Vector3 position.
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/MovingTheRoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTheRoad : MonoBehaviour
+{
+    public float speed;
+    Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(0, Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
