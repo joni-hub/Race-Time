f4c91499f702c8252dfb0c810fdc00a56381f5f9
Rajat Gupta <imrajatgupta@gmail.com>
2019-03-10 21:36:14 -0400

    Pause btn finally works

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        if(Time.timeScale == 1.0f)
+        {
+            Time.timeScale = 0;
+        } else if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
